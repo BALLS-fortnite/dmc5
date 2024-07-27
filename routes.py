ca982d768ba36f037812e8c24c6171f40d54eb53
BALLS-fortnite <21033@burnside.school.nz>
2024-07-27 19:54:45 +1200

    function to execute queries, save space\

--- routes.py
+++ routes.py
@@ -4,6 +4,21 @@ import sqlite3
 app = Flask(__name__)
 
 
+# do queries
+def execute_query(query, query_value=(), fetchone=False):
+    conn = sqlite3.connect('dmc5.db')
+    cur = conn.cursor()
+    cur.execute(query, query_value)
+
+    if fetchone:
+        result = cur.fetchone()
+    else:
+        result = cur.fetchall()
+
+    conn.close()
+    return result
+
+
 # base page
 @app.route('/')
 def homepage():
@@ -19,50 +34,35 @@ def about():
 # character page
 @app.route('/character/<int:id>')
 def character(id):
-    conn = sqlite3.connect('dmc5.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Character WHERE CharacterID=?', (id,))
-    character = cur.fetchone()
+    character = execute_query('SELECT * FROM Character WHERE CharacterID=?', (id,), fetchone=True)
     return render_template('character.html', character=character)
 
 
 # all characters
 @app.route('/allcharacters')
 def all_characters():
-    conn = sqlite3.connect('dmc5.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Character ORDER BY CharacterID')
-    all_characters = cur.fetchall()
+    all_characters = execute_query('SELECT * FROM Character ORDER BY CharacterID')
     return render_template('allcharacters.html', all_characters=all_characters)
 
 
 # all enemies
 @app.route('/allenemies')
 def all_enemies():
-    conn = sqlite3.connect('dmc5.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Enemy ORDER BY EnemyID')
-    all_enemies = cur.fetchall()
+    all_enemies = execute_query('SELECT * FROM Enemy ORDER BY EnemyID')
     return render_template('allenemies.html', all_enemies=all_enemies)
 
 
 # enemy page
 @app.route('/enemy/<int:id>')
 def enemy(id):
-    conn = sqlite3.connect('dmc5.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Enemy WHERE EnemyID=?', (id,))
-    enemy = cur.fetchone()
+    enemy = execute_query('SELECT * FROM Enemy WHERE EnemyID=?', (id,), fetchone=True)
     return render_template('enemy.html', enemy=enemy)
 
 
 # enemies by type
 @app.route('/enemy/type/<int:id>')
 def enemy_type(id):
-    conn = sqlite3.connect('dmc5.db')       
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM Enemy WHERE EnemyType=? ORDER BY EnemyID', (id,))
-    enemy_type = cur.fetchall()
+    enemy_type = execute_query('SELECT * FROM Enemy WHERE EnemyType=? ORDER BY EnemyID', (id,))
     return render_template('enemy_type.html', enemy_type=enemy_type)
 
 # @app.route('/triangles/<int:size>')
