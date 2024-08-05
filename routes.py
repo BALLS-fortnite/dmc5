878ef2e839a29a9cacede4e4ff0ba00682bfbbb8
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-06 11:43:06 +1200

    many to many table querie functioning, route needs to be formatted correctly

--- routes.py
+++ routes.py
@@ -86,13 +86,36 @@ def enemy_type(id):
 
 
 # select character to see all strategies for said character
-@app.route('/strategy/character/<int:id>')
-def character_strategy(id):
-    character_strategy = execute_query('SELECT * FROM Character WHERE CharacterID=?', (id,))
+@app.route('/strategy/character/')
+def character_strategy():
+    character_strategy = execute_query('SELECT * FROM Character ORDER BY CharacterID')
     if character_strategy == empty_query:
         return render_template('404.html')
     else:
-        return render_template('layout.html', character_strategy=character_strategy)
+        return render_template('character_strategy.html', character_strategy=character_strategy)
+
+
+# enemies by type
+@app.route('/strategy/character/<int:id>')
+def character_all_strategy(id):
+    character_all_strategy = execute_query('''SELECT 
+    Character.CharacterID, 
+    Character.CharacterName,
+    Character.CharacterIcon,
+    Character_Enemy.Difficulty,
+    Character_Enemy.Strategy,
+    Enemy.EnemyID,
+    Enemy.EnemyName,
+    Enemy.EnemyIcon,
+    Enemy.EnemyType
+FROM Character
+JOIN Character_Enemy ON Character.CharacterID = Character_Enemy.CharacterID
+JOIN Enemy ON Enemy.EnemyID = Character_Enemy.EnemyID
+WHERE Character.CharacterID = ?''', (id,))
+    if character_all_strategy == empty_query:
+        return render_template('404.html')
+    else:
+        return render_template('character_strategies.html', character_all_strategy=character_all_strategy)
 
 
 # error page
