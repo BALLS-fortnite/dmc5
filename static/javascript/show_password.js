3fbc1331313b73ea6f476d799f2e211b489257cf
KevinHuangNewZealand <kevin.huangnzc@gmail.com>
2024-09-22 18:12:21 +1200

    show and hide passwrod is a symbol inside the box

--- static/javascript/show_password.js
+++ static/javascript/show_password.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const togglePassword = document.querySelector("#togglePassword");
     const passwordInput = document.querySelector("#password");
-    const passwordRepeatInput = document.querySelector("#password_repeat");
 
     togglePassword.addEventListener("click", function() {
         const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
         passwordInput.setAttribute("type", type);
-        passwordRepeatInput.setAttribute("type", type);
-        this.textContent = type === "password" ? "Show Password" : "Hide Password"; // Change button text
+
+        // Toggle the image
+        this.src = type === "password" ? "/static/images/visibility.png" : "/static/images/visibility_off.png";
     });
 });
