32bfefd54cf9c33a6a5b0d560374892298dc9d28
KevinHuangNewZealand <kevin.huangnzc@gmail.com>
2024-09-22 19:42:11 +1200

    centered flash message and styled the login and register pages with flexbox

--- static/javascript/show_password.js
+++ static/javascript/show_password.js
@@ -2,11 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     const togglePassword = document.querySelector("#togglePassword");
     const passwordInput = document.querySelector("#password");
 
+    const togglePasswordRepeat = document.querySelector("#togglePasswordRepeat");
+    const passwordRepeatInput = document.querySelector("#password_repeat")
+
     togglePassword.addEventListener("click", function() {
         const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
         passwordInput.setAttribute("type", type);
 
-        // Toggle the image
+        // change the image
+        this.src = type === "password" ? "/static/images/visibility.png" : "/static/images/visibility_off.png";
+    });
+    // toggle for repeat password input box
+    togglePasswordRepeat.addEventListener("click", function() {
+        const type = passwordRepeatInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordRepeatInput.setAttribute("type", type);
+
+        // change the image
         this.src = type === "password" ? "/static/images/visibility.png" : "/static/images/visibility_off.png";
     });
 });
